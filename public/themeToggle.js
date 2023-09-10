148223af27cc594141c080b64019c46fe5d43367
Molly Meyer <ChaosCat7@protonmail.com>
2023-09-10 17:55:16 -0500

    Corrected html, css, & JS based on Linter feedback

--- public/themeToggle.js
+++ public/themeToggle.js
@@ -1,8 +1,6 @@
-// Allows the user to toggle between 'light' mode and 'dark' mode
+const button = document.querySelector('#theme-toggle')
 
-const button = document.querySelector('#theme-toggle');
-
-button.addEventListener('click', function() {
-  document.body.classList.toggle('dark');
-  button.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+button.addEventListener('click', function () {
+  document.body.classList.toggle('dark')
+  button.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙'
 })
