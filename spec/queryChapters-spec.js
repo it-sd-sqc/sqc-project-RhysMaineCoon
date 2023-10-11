ab89c65892599732ded12c01c8e77c4e807ca0b2
Bhayes9 <Bradleyhayes1999@gmail.com>
2023-10-11 15:05:33 -0500

    added jasmine tests and configured package.json for jasmine testing

---
+++ spec/queryChapters-spec.js
@@ -0,0 +1,26 @@
+import { queryChapters } from '../server.js'
+
+describe('queryChapters()', function () {
+  // check if chapters return as array // implemented by Bradley Hayes //
+  it('should return chapters as array', async function () {
+    const chapters = await queryChapters()
+    expect(chapters).toBeDefined()
+    expect(Array.isArray(chapters)).toBe(true)
+  })
+  // check if chapters return with expected properties //
+  it('should return expected properties from chapters', async function () {
+    const chapters = await queryChapters()
+    if (chapters.length > 0) {
+      const chapter = chapters[0]
+      expect(chapter).toHaveProperty('id')
+      expect(chapter).toHaveProperty('title')
+    }
+  })  
+  // End of implementation /////////////////////////////////
+})
+
+describe('example test', function () {
+  it('should pass', function () {
+    expect(true).toBe(true);
+  })
+})
