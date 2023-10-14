649144833f8096a5df7071e6c4ac9ed5fb87538d
Molly Meyer <ChaosCat7@protonmail.com>
2023-10-14 11:46:36 -0500

    Fixed Express Routes

--- server.js
+++ server.js
@@ -54,38 +54,6 @@ export const query = async function (sql, params) {
   .set('views', 'views')
   .set('view engine', 'ejs')
 
-
-
-// Routes //////////////////////////////////////////////////
-app.get('/', function (req, res) {
-    res.render('pages/index')
-  })
-  .get('/about', function (req, res) {
-    res.render('pages/about', { title: 'About' })
-  })
-.get('/index', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM chapters');
-        res.render('pages/chapters', { chapters: result.rows });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
-})
-.get('/index/:ch', async (req, res) => {
-    const chapterId = req.params.ch;
-    try {
-        const result = await pool.query('SELECT * FROM chapters WHERE id = $1', [chapterId]);
-        if (result.rows.length === 0) {
-            return res.status(404).send('Chapter not found');
-        }
-        res.render('pages/chapter', { chapter: result.rows[0] });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
-});
-
 // Web server setup ////////////////////////////////////////
 const app = express()
 app.use(express.static('public'))
@@ -94,5 +62,18 @@ app.use(express.static('public'))
 const displayPort = function () {
   console.log('Listening on ' + PORT)
 };
+
+// Express Routes/////////////////////////////////////////////////
+express()
+  .use(express.static('./public/'))
+  .set('views', 'views')
+  .set('view engine', 'ejs')
+  .get('/', function (req, res) {
+    res.render('pages/index')
+  })
+  .get('/about', function (req, res) {
+    res.render('pages/about')
+  })
+  
 //app.listen(PORT, displayPort);
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
