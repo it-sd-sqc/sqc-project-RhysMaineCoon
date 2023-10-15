805cfa24c9e3d4cfdef68171c315932220a8d618
Molly Meyer <ChaosCat7@protonmail.com>
2023-10-15 12:39:47 -0500

    Updated Id tags and ejs files

--- server.js
+++ server.js
@@ -4,78 +4,75 @@ import express from 'express'
 import pkg from 'pg'
 const { Pool } = pkg
 
-
 // Configuration ///////////////////////////////////////////
 const PORT = process.env.PORT || 5163
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 // Query functions /////////////////////////////////////////
 export const query = async function (sql, params) {
-    let client
-    let results = []
-    try {
-      client = await pool.connect()
-      const response = client.query(sql, params)
-      if (response && response.rows) {
-        results = response.rows
-      }
-    } catch (err) {
-      console.error(err)
-    }
-    if (client) client.release()
-    return results
+  const client = await pool.connect()
+  try {
+    const response = await client.query(sql, params)  // Added await here
+    return response.rows || []
+  } catch (err) {
+    console.error(err)
+    return []
+  } finally {
+    client.release()
   }
-  /*
-  export const getChapter = async function () {
-    const data = await query('SELECT chapter_id, title, number FROM section;')
-    const table = {}
-    data.forEach((row) => {
-      if (row.number === 0) {
-        table[row.chapter_id] = {}
-      }
-      table[row.chapter_id][row.number] = row.title
-    })
-  
-    return table
-  }
-  */
-  
-  export const getChapters = async function (id) {
-    const sql = `SELECT section.title, section.number AS sect_num, element.section_id, element.content, element.index, element.element_type_id AS type
-    FROM section RIGHT JOIN element ON section.id = element.section_id
-    WHERE section.chapter_id = ${number}
-    ORDER BY section_id ASC, index ASC;`
-    return await query(sql)
-  }
+}
 
-// Configure web server & Express Routes////////////////////////////////
-  express()
-  .use(express.static('public'))
-  .set('views', 'views')
-  .set('view engine', 'ejs')
+export const getChapters = async function (id) {  // Changed parameter to id
+  const sql = `SELECT section.title, section.number AS sect_num, element.section_id, 
+               element.content, element.index, element.element_type_id AS type
+               FROM section RIGHT JOIN element ON section.id = element.section_id
+               WHERE section.chapter_id = $1
+               ORDER BY section_id ASC, index ASC;`
+  return await query(sql, [id])  // Parameterized query
+}
 
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .get('/', function (req, res) {
-    res.render('pages/index')
-  })
-  .get('/about', function (req, res) {
-    res.render('pages/about')
-  })
-
-// Web server setup ////////////////////////////////////////
+// Initialize Express App /////////////////////////////////
 const app = express()
-app.use(express.static('public'))
+app.use(express.static('./public/'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
 
-// Ready for browsers to connect ///////////////////////////
-/* const displayPort = function () {
-  console.log('Listening on ' + PORT)
-};
-*/
+// Routes /////////////////////////////////////////////////
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/chapter/:chapterId', async (req, res) => {
+  try {
+    const { chapterId } = req.params
+    const chapterData = await query('SELECT * FROM chapters WHERE id = $1', [chapterId])
+    res.render('chapterPage', { chapter: chapterData[0] })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+app.get('/chapters', async (req, res) => {
+  try {
+    const allChapters = await query('SELECT * FROM chapters')
+    res.render('chaptersPage', { chapters: allChapters })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+// Start Server ///////////////////////////////////////////
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`)
+})
